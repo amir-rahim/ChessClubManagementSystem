a5cd49d849d501a78d3cd1b96537a2aa51275f26
Riccardo Gugliermini <riccardo.gugliermini@kcl.ac.uk>
2021-11-18 08:39:18 +0000

    Update seeder and unseeder

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,19 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
-from clubs.models import User, Club
+from clubs.models import User, Club, Membership
 
 class Command(BaseCommand):
     """The database seeder."""
 
     def handle(self, *args, **options):
-        user = User.objects.create_user(username = "jkerman", password = "Password123.")
-        user.username = "jkerman"
-        user.name = "Jebediah Kerman"
-        user.email = "jeb@example.org"
-        user.save()
+        user1 = User.objects.create_user(username = "jkerman", password = "Password123.")
+        user1.username = "jkerman"
+        user1.name = "Jebediah Kerman"
+        user1.email = "jeb@example.org"
+        user1.save()
 
-        user = User.objects.create_user(username = "bkerman", password = "Password123.")
-        user.name = "Billie Kerman"
-        user.email = "billie@example.org"
-        user.save()
-        
-        Club.objects.create(name = "Kerbal Chess Club", owner = user)
+        user2 = User.objects.create_user(username = "bkerman", password = "Password123.")
+        user2.name = "Billie Kerman"
+        user2.email = "billie@example.org"
+        user2.save()
+
+        club1 = Club.objects.create(name = "Kerbal Chess Club", owner = user2)
+
+        Membership.objects.create(user = user2, club = club1)
--- clubs/management/commands/unseed.py
+++ clubs/management/commands/unseed.py
@@ -1,11 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
-from clubs.models import User, Club
+from clubs.models import User, Club, Membership
 
 class Command(BaseCommand):
     """The database unseeder."""
 
     def handle(self, *args, **options):
-        user1 = User.objects.get(username="bkerman")
-        Club.objects.get(name="Kerbal Chess Club", owner=user1).delete()
+        user2 = User.objects.get(username="bkerman")
+        club1 = Club.objects.get(name = "Kerbal Chess Club", owner = user2)
+
+
+        Membership.objects.get(user = user2, club = club1).delete()
+
+        Club.objects.get(name="Kerbal Chess Club", owner=user2).delete()
+
         User.objects.get(username="jkerman").delete()
         User.objects.get(username="bkerman").delete()
