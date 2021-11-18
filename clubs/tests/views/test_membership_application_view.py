2b3bc571ae36a30143658f4178269a3a95bc326b
Riccardo Gugliermini <riccardo.gugliermini@kcl.ac.uk>
2021-11-18 09:40:59 +0000

    Add test to membership form view

--- clubs/tests/views/test_membership_application_view.py
+++ clubs/tests/views/test_membership_application_view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.hashers import check_password
 from django.test import TestCase
 from django.urls import reverse
 from clubs.forms import MembershipApplicationForm
-from clubs.models import User, Club
+from clubs.models import User, Club, Membership
 from clubs.tests.helpers import LogInTester, reverse_with_next
 
 class MembershipApplicationViewTestCase(TestCase, LogInTester):
@@ -17,10 +17,11 @@ class MembershipApplicationViewTestCase(TestCase, LogInTester):
     def setUp(self):
         self.url = reverse('membership_application')
         self.user = User.objects.get(username='johndoe')
+        self.applicant = User.objects.get(username='janedoe')
         self.club = Club.objects.get(name='Kerbal Chess Club')
         self.form_input = {
             'club' : self.club,
-            'user' : self.user
+            'user' : self.applicant
         }
 
     def test_membership_application_url(self):
@@ -42,3 +43,43 @@ class MembershipApplicationViewTestCase(TestCase, LogInTester):
         redirect_url = reverse_with_next('log_in', self.url)
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
+    def test_owner_cannot_apply_own_club(self):
+        self.client.login(username="johndoe", password="Password123")
+        response = self.client.get(self.url)
+        form = response.context['form']
+        form.cleaned_data = self.form_input
+        form.save()
+        self.assertFalse(form.is_valid())
+
+    def test_form_shows_all_clubs(self):
+        self.client.login(username="johndoe", password="Password123")
+        response = self.client.get(self.url)
+        form = response.context['form']
+        self.assertEqual(len(form['club'].field.queryset), Club.objects.count())
+
+    def test_apply_to_club(self):
+        self.client.login(username="janedoe", password="Password123")
+        response = self.client.get(self.url)
+        form = response.context['form']
+        form.cleaned_data = self.form_input
+        before_count = Membership.objects.count()
+        form.save()
+        after_count = Membership.objects.count()
+        self.assertEqual(after_count, before_count + 1)
+
+    def test_cannot_apply_twice(self):
+        self.client.login(username="janedoe", password="Password123")
+        response = self.client.get(self.url)
+        form = response.context['form']
+        form.cleaned_data = self.form_input
+        before_count = Membership.objects.count()
+        form.save()
+        after_count = Membership.objects.count()
+        self.assertEqual(after_count, before_count + 1)
+
+        response = self.client.get(self.url)
+        form = response.context['form']
+        form.cleaned_data = self.form_input
+        form.save()
+        self.assertFalse(form.is_valid())
