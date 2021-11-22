21982a8e4caa8d6386479e7874a4a166e5c9b0e5
Jake Davis <jake.davis@kcl.ac.uk>
2021-11-22 17:50:04 +0000

    Add Club Dashboard View Tests

--- clubs/tests/views/test_club_dashboard_view.py
+++ clubs/tests/views/test_club_dashboard_view.py
@@ -16,6 +16,7 @@ class ClubDashboardViewTestCase(TestCase):
     def setUp(self):
         self.club = Club.objects.get(id=1)
         self.user = User.objects.get(username='johndoe')
+        self.officer = User.objects.get(username='jonhatandoe')
 
     def test_get_club_dashboard_view(self):
         self.client.login(username=self.user.username, password="Password123")
@@ -30,7 +31,7 @@ class ClubDashboardViewTestCase(TestCase):
         redirect_url = reverse_with_next('log_in', url)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
-        self.assertRedirects(response, redirect_url,     status_code=302, target_status_code=200)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
     def test_club_dashboard_view_owner_context(self):
         self.client.login(username=self.user.username, password="Password123")
@@ -41,22 +42,32 @@ class ClubDashboardViewTestCase(TestCase):
         self.assertTemplateUsed(response, 'club_dashboard.html')
         self.assertEqual(response.context['is_owner'], True)
 
-    def test_club_dashboard_view_member_context(self):
+    def test_club_dashboard_view_officer_context(self):
+        self.client.login(username=self.officer.username, password="Password123")
+
+        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'club_dashboard.html')
+        self.assertEqual(response.context['is_officer'], True)
+ 
+    def test_club_dashboard_view_member(self):
         self.client.login(username=self.user.username, password="Password123")
 
         url = reverse('club_dashboard', kwargs={'id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
-        self.assertEqual(response.context['is_member'], True)
 
-    def test_club_dashboard_non_member_context(self):
+    def test_club_dashboard_non_member_redirects(self):
         user = User.objects.get(username='janedoe')
+        
         club = Club.objects.get(id=2)
         self.client.login(username=user.username, password="Password123")
 
         url = reverse('club_dashboard', kwargs={'id': club.id})
+        redirect_url = reverse('user_dashboard')
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'club_dashboard.html')
-        self.assertEqual(response.context['is_member'], False)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
+
