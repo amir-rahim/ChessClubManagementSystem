815039b6437591fdc34e48d1b711650bc6aef82f
Jessy BRIARD <jessy.briard@kcl.ac.uk>
2021-12-01 18:06:24 +0100

    Fix broken tests

--- clubs/tests/views/test_club_dashboard_view.py
+++ clubs/tests/views/test_club_dashboard_view.py
@@ -21,13 +21,13 @@ class ClubDashboardViewTestCase(TestCase):
     def test_get_club_dashboard_view(self):
         self.client.login(username=self.user.username, password="Password123")
 
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
 
     def test_club_dashboard_view_redirects_not_logged_in(self):
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         redirect_url = reverse_with_next('log_in', url)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
@@ -36,7 +36,7 @@ class ClubDashboardViewTestCase(TestCase):
     def test_club_dashboard_view_member(self):
         self.client.login(username=self.user.username, password="Password123")
 
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
@@ -44,7 +44,7 @@ class ClubDashboardViewTestCase(TestCase):
     def test_pending_applications_hidden_to_non_officer(self):
         user2 = User.objects.get(username="janedoe")
         self.client.login(username=user2.username, password="Password123")
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
@@ -52,7 +52,7 @@ class ClubDashboardViewTestCase(TestCase):
 
     def test_pending_applications_no_pending_application(self):
         self.client.login(username=self.officer.username, password="Password123")
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
@@ -66,7 +66,7 @@ class ClubDashboardViewTestCase(TestCase):
         user2 = User.objects.get(username='janedoe')
         new_membership = Membership(user=user2, club=self.club, user_type="NM", application_status='P')
         new_membership.save()
-        url = reverse('club_dashboard', kwargs={'id': self.club.id})
+        url = reverse('club_dashboard', kwargs={'club_id': self.club.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_dashboard.html')
