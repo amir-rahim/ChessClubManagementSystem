8df6112800eddfbfa3032aabca78b6a48435cd85
Amir Rahim <amir.rahim@kcl.ac.uk>
2021-12-09 14:16:28 +0000

    Implement User, Club and Membership Seeder

--- clubs/management/commands/seed.py
+++ clubs/management/commands/seed.py
@@ -1,10 +1,39 @@
 from django.core.management.base import BaseCommand, CommandError
-from clubs.models import User, Club, Membership
+
+from clubs.models import User, Club, Membership, Tournament, TournamentParticipation
+
+import pytz
+from faker import Faker
+from random import randint, random
 
 class Command(BaseCommand):
     """The database seeder."""
 
+    USER_COUNT = 100
+    CLUB_COUNT = 20
+    TOURNAMENT_COUNT = 5
+    MEMBER_CHANCE = 0.2
+    APPROVE_CHANCE = 0.9
+    OFFICER_CHANCE = 0.1
+    DEFAULT_PASSWORD = 'Password123'
+    help = 'Seeds the database with sample data'
+
+    def __init__(self):
+        self.faker = Faker('en_GB')
+
     def handle(self, *args, **options):
+        self.create_default_users()
+        self.create_users()
+        self.users = User.objects.exclude(is_superuser=True)
+        self.create_clubs()
+        self.clubs = Club.objects.all()
+        self.create_default_memberships()
+        self.make_memberships()
+        self.memberships = Membership.objects.all()
+        # self.create_tournaments()
+        # self.tournaments = Tournaments.objects.all()
+
+    def create_default_users(self):
         user1 = User.objects.create_user(username = "jkerman", password = "Password123")
         user1.name = "Jebediah Kerman"
         user1.email = "jeb@example.org"
@@ -26,20 +55,148 @@ class Command(BaseCommand):
         user3.chess_experience = "A"
         user3.save()
 
+        print("Default Users seeding complete.      ")
+
         club1 = Club.objects.create(name = "Kerbal Chess Club", owner = user3)
         club1.location = "London, United Kingdom"
         club1.mission_statement = "We have the best players in the world!"
         club1.description = "We are a club to develop the best players in the world!"
         club1.save()
 
-        membership1 = Membership.objects.create(user = user2, club = club1)
-        membership1.personal_statement = "I want to join this club!"
-        membership1.application_status = "A"
-        membership1.user_type = "OF"
-        membership1.save()
+        print("Default Club seeding complete.      ")
 
-        membership2 = Membership.objects.create(user = user1, club = club1)
-        membership2.personal_statement = "I really like this club!"
-        membership2.application_status = "A"
-        membership2.user_type = "MB"
-        membership2.save()
+
+    def create_users(self):
+        user_count = 3
+        while user_count < self.USER_COUNT:
+            print(f"Seeding user {user_count}/{self.USER_COUNT}", end='\r')
+            try:
+                self.create_user_profile()
+            except:
+                continue
+            user_count = user_count + 1
+        print("User seeding complete.      ")
+
+    def create_user_profile(self):
+        first_name = self.faker.first_name()
+        last_name = self.faker.last_name()
+        name = first_name + " " + last_name
+        email = self.create_email(first_name, last_name)
+        username = self.create_username(first_name, last_name)
+        public_bio = self.faker.text(max_nb_chars=250)
+        chess_experience = 'B'
+        random_chess_experience = random()
+
+        if random_chess_experience <= 0.20:
+            chess_experience = 'B'
+        elif random_chess_experience <= 0.40:
+            chess_experience = 'I'
+        elif random_chess_experience <= 0.60:
+            chess_experience = 'A'
+        elif random_chess_experience <= 0.80:
+            chess_experience = 'M'
+        else:
+            chess_experience = 'G'
+
+        user_create = User.objects.create_user(username=username, password=Command.DEFAULT_PASSWORD)
+        user_create.name=name
+        user_create.email=email
+        user_create.public_bio=public_bio,
+        user_create.chess_experience=chess_experience
+
+        user_create.save()
+
+    def create_email(self, first_name, last_name):
+        return first_name + '.' + last_name + '@example.org'
+
+    def create_username(self, first_name, last_name):
+        return first_name.lower() + last_name.lower()
+
+    def get_random_user(self):
+        index = randint(0,self.users.count()-1)
+        return self.users[index]
+
+    def create_clubs(self):
+        club_count = 2
+        while club_count < self.CLUB_COUNT:
+            print(f"Seeding club {club_count}/{self.CLUB_COUNT}", end='\r')
+            try:
+                self.create_club()
+            except:
+                continue
+            club_count = club_count + 1
+        print("Club seeding complete.      ")
+
+    def create_club(self):
+        name = self.faker.company()
+        owner = self.get_random_user()
+        location = self.faker.city()
+        mission_statement = self.faker.catch_phrase()
+        description = self.faker.text(max_nb_chars=500)
+
+        club = Club.objects.create(
+            name=name,
+            owner=owner,
+            location=location,
+            mission_statement=mission_statement,
+            description=description
+        )
+
+        club.save()
+
+    def make_memberships(self):
+        count = 1
+        for user in self.users:
+            for club in self.clubs:
+                if not Membership.objects.filter(club=club, user=user).exists():
+                    if random() < self.MEMBER_CHANCE:
+
+                        if random() < self.APPROVE_CHANCE:
+                            application_status = 'A'
+
+                            if random() < self.OFFICER_CHANCE:
+                                user_type = 'OF'
+                            else:
+                                user_type = 'MB'
+                        else:
+                            application_status = 'P'
+                            user_type = 'NM'
+                                
+
+                        print(f"Seeding memberships, Count:{count}", end='\r')
+
+                        personal_statement = self.faker.text(max_nb_chars=500)
+
+                        Membership.objects.create(
+                            user=user,
+                            club=club,
+                            personal_statement=personal_statement,
+                            application_status=application_status,
+                            user_type=user_type
+                        )
+
+        print("Memberships seeding complete.      ")
+
+    def create_default_memberships(self):
+
+        vkerman = self.users.get(name="Valentina Kerman")
+        jkerman = self.users.get(name="Jebediah Kerman")
+        kerbal = self.clubs.get(name="Kerbal Chess Club")
+
+        member_check1 = Membership.objects.create(
+                user = vkerman, 
+                club = kerbal,
+                personal_statement = "I want to join this club!",
+                application_status = "A",
+                user_type = "OF")
+        member_check2 = Membership.objects.create(
+                user = jkerman, 
+                club = kerbal,
+                personal_statement = "I really like this club!",
+                application_status = "A",
+                user_type = "MB")
+
+        member_check1.save()
+        member_check2.save()
+
+        print("Default Memberships seeding complete.      ")
