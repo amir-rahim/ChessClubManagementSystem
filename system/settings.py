84cfb5b0f3bd216fc748bfc4fe4a8e44f9fdf982
Amir Rahim <amir.rahim@kcl.ac.uk>
2021-12-13 12:12:02 +0000

    Configure Application for Deployment

--- system/settings.py
+++ system/settings.py
@@ -14,6 +14,11 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as message_constants
 
+# Activate django_heroku
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,6 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -148,3 +154,6 @@ MESSAGE_TAGS = {
     message_constants.WARNING: 'warning',
     message_constants.ERROR: 'danger'
 }
+
+# # Activate Django Heroku
+# django_heroku.settings(locals())
