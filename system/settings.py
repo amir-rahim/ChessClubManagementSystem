f63332bc3f7847e6d8a5a5af3faf93864d5e4f7b
Amir Rahim <amir.rahim@kcl.ac.uk>
2021-12-13 13:58:50 +0000

    Update settings.py

--- system/settings.py
+++ system/settings.py
@@ -14,11 +14,6 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as message_constants
 
-# Activate django_heroku
-if '/app' in os.environ['HOME']:
-    import django_heroku
-    django_heroku.settings(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -155,5 +150,7 @@ MESSAGE_TAGS = {
     message_constants.ERROR: 'danger'
 }
 
-# Activate Django Heroku
-django_heroku.settings(locals())
+# Activate django_heroku
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
